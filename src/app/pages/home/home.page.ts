c1b0183a7cb4a6404fd8b96d2ca519f904ee72cd
yvettebaker90 <yvettebaker90@gmail.com>
2026-03-25 16:07:49 +0100

    added routing so the footer shows up on the pages

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ProductCardsComponent } from '../../components/product-card/product-card.component';
 
+
 @Component({
   selector: 'app-home',
   imports: [ProductCardsComponent],
