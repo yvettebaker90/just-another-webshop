17a35746a9809b0c020175d58c7c4c7620bf1b14
sali1502 <sali1502@sudent.miun.se>
2026-03-26 14:47:20 +0100

    added basic layout to header with icons

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,14 +1,10 @@
 import { Component } from '@angular/core';
-
 import { ProductCardsComponent } from '../../components/product-card/product-card.component';
-import { SupabaseTestComponent } from '../../components/supabase-test/supabase-test.component';
-
-
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ProductCardsComponent, SupabaseTestComponent],
+  imports: [ProductCardsComponent],
   templateUrl: './home.page.html'
 })
 export class Home {}
