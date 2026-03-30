ec0be414feacc2648d61b3baaa7d1b14d7c582b2
yvettebaker90 <yvettebaker90@gmail.com>
2026-03-30 09:09:00 +0200

    added a product details page to route but no layout yet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,16 @@ import { Home } from './pages/home/home.page';
 import { Products } from './pages/products/products.page';
 import { ProductDetail } from './pages/products-detail/products-details.page';
 import { ShoppingCart } from './pages/shopping-cart/shopping-cart.page';
+import { ProductDetails } from './components/product-details/product-details.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: Home },
+  
   { path: 'products', component: Products },
   { path: 'products/:id', component: ProductDetail },
   { path: 'shopping-cart', component: ShoppingCart },
-  { path: '**', redirectTo: 'products' }
+  { path: 'product-details', component: ProductDetails },
+  { path: 'home', component: Home },
+  { path: '**', redirectTo: 'products' },
+  
 ];
