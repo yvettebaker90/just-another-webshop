101e78b4cc7b9fce2b20e22f8cc8f7121ed56199
sali1502 <sali1502@sudent.miun.se>
2026-04-01 11:58:02 +0200

    added searchbar to header

--- src/app/components/product-card/navigation/header.component.ts
+++ src/app/components/product-card/navigation/header.component.ts
@@ -3,12 +3,13 @@ import { NgIcon, provideIcons } from '@ng-icons/core';
 import { ionHeartOutline, ionCartOutline, ionPersonOutline, ionHomeOutline, ionBagOutline } from '@ng-icons/ionicons';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { SearchBarComponent } from '../../searchbar/searchbar.component';
 
 @Component({
   selector: 'app-header',
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [NgIcon, RouterModule, CommonModule],
+  imports: [NgIcon, RouterModule, SearchBarComponent, CommonModule],
   providers: [provideIcons({ ionHeartOutline, ionCartOutline, ionPersonOutline, ionHomeOutline, ionBagOutline })],
   templateUrl: './header.component.html',
 })
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { ProductCardsComponent } from '../../components/product-card/product-card.component';
-import { SearchBarComponent } from '../../components/searchbar/searchbar.component';
 import { HeroComponent } from '../../components/hero/hero.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ProductCardsComponent, SearchBarComponent, HeroComponent],
+  imports: [ProductCardsComponent, HeroComponent],
   templateUrl: './home.page.html'
 })
 export class Home {}
